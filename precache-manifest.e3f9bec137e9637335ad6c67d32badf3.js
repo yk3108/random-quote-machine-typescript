23ba2b5956df8cf2a17f6e9ca82e197df245aaa0
yk3108 <66248058+yk3108@users.noreply.github.com>
2020-09-24 16:30:34 +0900

    Updates

---
+++ precache-manifest.e3f9bec137e9637335ad6c67d32badf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c812b99f937fae82fa03aae06497d9",
+    "url": "/random-quote-machine-typescript/index.html"
+  },
+  {
+    "revision": "b4c66ca9f25b1b187e8b",
+    "url": "/random-quote-machine-typescript/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "18ffd328a6d2a31bb3bc",
+    "url": "/random-quote-machine-typescript/static/css/main.1a34d0ff.chunk.css"
+  },
+  {
+    "revision": "b4c66ca9f25b1b187e8b",
+    "url": "/random-quote-machine-typescript/static/js/2.989292e2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/random-quote-machine-typescript/static/js/2.989292e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ffd328a6d2a31bb3bc",
+    "url": "/random-quote-machine-typescript/static/js/main.27cd25d0.chunk.js"
+  },
+  {
+    "revision": "ca000c9ee1c6c73315f1",
+    "url": "/random-quote-machine-typescript/static/js/runtime-main.d4cfe398.js"
+  }
+]);
